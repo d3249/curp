0616db7fa30feada21890119fc4e0cce26e5cef4
Edgar Arturo Ramos Rambaud <edgar.a.ramos.rambaud@gmail.com>
2018-06-18 09:37:11 -0500

    Primeros pasos para la implementación en Java

---
+++ java/src/main/java/per/d3249/curp/CURPGenerator.java
@@ -0,0 +1,7 @@
+package per.d3249.curp;
+
+public class CURPGenerator {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ java/src/main/java/per/d3249/curp/Catalogos.java
@@ -0,0 +1,19 @@
+package per.d3249.curp;
+
+public class Catalogos {
+
+    public static final String[] PRIMEROS_NOMBRES_IGNORADOS = { "MARIA", "MA.", "MA", "JOSE", "J", "J.", "DA", "DAS",
+            "DE", "DEL", "DER", "DI", "DIE", "DD", "EL", "LA", "LOS", "LAS", "LE", "LES", "MAC", "MC", "VAN", "VON",
+            "Y" };
+
+    public static final String[] PALABRAS_IMPROPIAS = { "BACA", "BAKA", "BUEI", "BUEY", "CACA", "CACO", "CAGA", "CAGO",
+            "COGI", "COJA", "COJE", "COJI", "COJO", "COLA", "CULO", "FALO", "FETO", "GETA", "GUEI", "GUEY", "JETA",
+            "JOTO", "KACA", "KACO", "KAGA", "KAGO", "KAKA", "KAKO", "KOGE", "KOGI", "KOJA", "KOJE", "KOJI", "KOJO",
+            "KOLA", "KULO", "LILO", "LOCA", "LOCO", "LOKA", "LOKO", "MAME", "MAMO", "MEAR", "MEAS", "MEON", "MIAR",
+            "MION", "MOCO", "MOKO", "MULA", "MULO", "NACA", "NACO", "PEDA", "PEDO", "PENE", "PIPI", "PITO", "POPO",
+            "PUTA", "PUTO", "QULO", "RATA", "ROBA", "ROBE", "ROBO", "RUIN", "SENO", "TETA", "VACA", "CAGA", "VAGO",
+            "VAKA", "VUEI", "VUEY", "WUEI", "WUEY" };
+
+    private Catalogos() {
+    };
+}
